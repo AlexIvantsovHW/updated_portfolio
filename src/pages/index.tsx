8b1f8aa6bd67a9e0b686fd4b0745ce5cface5157
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2024-09-21 16:26:46 +0300

    feat:test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "@mui/material/styles";
-import "@mui/material/Button";
-import App from "./App.tsx";
-import "../index.css";
-import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
-import { Provider } from "react-redux";
-import { store } from "../app/store/store.ts";
-import { GRAPHQL_URL } from "../app/api/api.ts";
+import '@mui/material/styles'
+import '@mui/material/Button'
+import '../index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client'
+import { Provider } from 'react-redux'
+import { store } from '../app/store/store.ts'
+import { GRAPHQL_URL } from '../app/api/api.ts'
+import App from './App.tsx'
 
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
-});
-createRoot(document.getElementById("root")!).render(
+})
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ApolloProvider client={client}>
         <App />
       </ApolloProvider>
     </Provider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
